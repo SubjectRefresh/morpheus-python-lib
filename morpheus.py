0c78cc9965a09a70e740b156df7429ec688c01e8
Finnian Anderson <finnian@fxapi.co.uk>
2015-12-22 19:14:37 +0000

    Cleanup and add documentation

--- morpheus.py
+++ morpheus.py
@@ -2,16 +2,16 @@ import urllib.parse
 import urllib.request
 import json
 
-def morpheus(fr, to, ur):
+def convert(fr, to, ur):
+    """ this function returns the converted text """
+
     url = 'http://subjectrefresh.info/morpheus/api/convert'
     values = {'from' : fr,
               'to' : to,
               'url' : ur }
 
     data = urllib.parse.urlencode(values)
-    print(data)
     full_url = url + '?' + data
-    print(full_url)
     
     content = urllib.request.urlopen(full_url)
     output = content.read().decode('utf-8')
@@ -19,24 +19,3 @@ def morpheus(fr, to, ur):
     for item in ser:
         if ser[item] != ("url" or "from" or "to"):
             return ser[item]
-        
-
-f = open('workfile', 'w')
-f.write(morpheus("pdf", "html", "http://www.indire.it/wp-content/uploads/2015/08/pdf-sample.pdf"))
-f.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
